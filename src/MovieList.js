2562cabeae56890bae629ee5391d4e3879b3a325
RianIslam <rianislamdr@gmail.com>
2021-02-09 02:33:04 +0600

    create api

---
+++ src/MovieList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const MovieList = () => {
+    const [movies,setMovies] = useState([
+        {
+            name:"Avengers",
+            price:"$10",
+            id:2020
+        },
+        {
+            name:"Game of Thrones",
+            price:"$20",
+            id:20303
+        },
+        {
+            name:"inception",
+            price:"$99",
+            id:39485
+        }
+    ]);
+    return (
+
+        <div>
+            
+        </div>
+    )
+}
+
+export default MovieList
