d17ad5c1e0b5cbc25d23ba1d6e12751b7e1148d6
RianIslam <rianislamdr@gmail.com>
2021-02-12 21:03:09 +0600

    await

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const exampleReq = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+  
 
   const [counter, setcounter] = useState(0);
 
-  useEffect(()=>{
-    console.log('Effect has been run')
-  })
+  useEffect(() => {
+    
+  }, []);
 
-
+  const getRecipes = async () =>{
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`);
+    const data =await response.json()
+    console.log(data)
+  }
   return (
- 
-
     <div className="App">
-    <form className="search__form">
-      <input className="search__bar" type="text"/>
-      <button className="search__button" type="submit">
-      Search
-      </button>
-      <h1 onClick={()=>setcounter(counter +1)}>{counter}</h1>
-    </form>
+      <form className="search__form">
+        <input className="search__bar" type="text" />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
-
-
   );
 }
 
