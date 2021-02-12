4cd371ec6b6aff2a84ef4727fece748839d759cb
RianIslam <rianislamdr@gmail.com>
2021-02-12 20:22:49 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
 
   const exampleReq = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+
+  const [counter, setcounter] = useState(0);
+
+  useEffect(()=>{
+    console.log('Effect has been run')
+  })
 
 
   return (
@@ -12,7 +19,9 @@ function App() {
     <div className="App">
     <form className="search__form">
       <input className="search__bar" type="text"/>
-      <button className="search__button" type="submit"/>
+      <button className="search__button" type="submit">
+      {counter}
+      </button>
     </form>
     </div>
 
