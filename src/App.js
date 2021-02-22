155c619f1dadf7c1c0e7ea3be01debad825fab35
RianIslam <rianislamdr@gmail.com>
2021-02-22 23:18:12 +0600

    besic react

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+function Person(){
+  const useIt ={
+    width :"200px",
+    border : "2px solid yellow",
+    margin : "2px"
+  }
+  return(
+    <div style={useIt}>
+      <h2>Name:</h2>
+      <p>Rian</p>
+    </div>
+  )
 }
 
 export default App;
