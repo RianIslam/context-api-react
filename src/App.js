3dc033c9094da33cddebeb247b34dc3aae1366bd
RianIslam <rianislamdr@gmail.com>
2021-02-23 00:10:35 +0600

    props add and some style add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <Person name="rian"></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Rian Islam" job="Software Engineer"></Person>
+      <Person name="Kabir Ahammed" job="Software"></Person>
     </div>
   );
 }
-function Person(props){
-  const useIt ={
-    width :"200px",
-    border : "2px solid yellow",
-    margin : "2px"
+
+function Person(props) {
+  const someColor ={
+    border: '2px solid blue',
+    margin: '5px',
+    width : '300px'
   }
-  return(
-    <div style={useIt}>
-      <h2>Name: Name={props.name}</h2>
-      <p>Rian</p>
+  return (
+    <div style={someColor}>
+      <h1>Name:{props.name}</h1>
+      <h4>Passion: {props.job}</h4>
     </div>
-  )
+  );
 }
 
 export default App;
