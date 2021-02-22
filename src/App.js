2f3121b567c112dee7ba3c5d6f4ab48ececbb5ef
RianIslam <rianislamdr@gmail.com>
2021-02-22 22:22:21 +0600

    react core concept

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Recipe from "./Recipe";
 
 function App() {
-  const [recipes,setRecipes] = useState([]);
-  useEffect(() => {
-    getRecipes();
-  }, []);
 
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
-    );
-    const data = await response.json();
-    setRecipes(data.meals[0]);
-    console.log(data.meals[0])
-  };
   return (
     <div className="App">
-      <form className="search__form">
-        <input className="search__bar" type="text" />
-        <button className="search__button" type="submit">
-          Search
-        </button>
-      </form>
-      {recipes.map(recipe=>(
-        <Recipe/>
-      ))}
+      
     </div>
   );
 }
