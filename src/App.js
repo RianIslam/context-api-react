5c9c397398f183962a3d105f6f8f90dee4e8e31f
RianIslam <rianislamdr@gmail.com>
2021-02-24 01:29:09 +0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
     
     <div className="App">
     <Counter></Counter>
+    <Users></Users>
       <ul>
         {
           nayok.map(nayok => <li>{nayok}</li>)
@@ -48,12 +49,30 @@ function Counter(){
     <div>
       <h1>Count : {count}</h1>
       {/* <button onClick={handleIncrease}>Increase</button> */}
-      <button onClick={()=> setCount(count+1)}>Increase</button>
+      <button onMouseMove={()=> setCount(count+1)}>Increase</button>
       <button onClick={()=> setCount(count - 1)}>Decrease</button>
     </div>
   )
 }
 
+function Users(){
+  const [users,setUsers] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+  return(
+    <div>
+     <h3>Dynamic Users : {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+ )
+}
 
 function Product(props){
   const productStyle = {
